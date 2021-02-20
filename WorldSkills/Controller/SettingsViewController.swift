51dd1024c3f366c62a6df6ab946605036b074b85
Jack <l.chakkei.jack@gmail.com>
2021-02-20 12:38:39 +0800

    update tabbar item name

---
+++ WorldSkills/Controller/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  WorldSkills
+//
+//  Created by Jack on 20/2/2021.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
